e14a308e1034a3e72b86dcbadf8f0abe685e3cb7
Kisan Majumdar <kisanmajumdar44@gmail.com>
2024-10-04 19:46:58 +0530

    Update db.js

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
-const URL = "mongodb+srv://movix:VishaL0202@cluster0.1jzvmro.mongodb.net/movix"
+const mongoose = require("mongoose");
+const URL = "mongodb://localhost:27017";
 
-const connectDB  = async () => {
-    try {
-        await mongoose.connect(URL);
-        console.log("MongoDB connected");
-    } catch (err) {
-        console.log(err);
-        process.exit(1);
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(URL);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
